df70dbf8e03ae4b1d40ae1af86b2924c3cfc357e
mrg123 <ccna8008@163.com>
2018-04-26 22:03:13 +0800

    0

--- mok/UI/weixin/script/ok_pay.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    var $pay = {
-        init:function(){
-            var _W = $(window).width() > 750 ? 750 : $(window).width();
-            $('html').css('fontSize',_W/7.5+'px');
-        }
-    };
-    $pay.init();
-});
--- mok/UI/weixin/script/ok_play.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-    var $width = $(window).width()>750?750:$(window).width();
-    $('html').css('fontSize',$width/7.5+'px');
-});
